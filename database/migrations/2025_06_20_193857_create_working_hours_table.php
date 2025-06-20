71f4a0a24ede7355daeee5cf2158373beec17db3
elmaaref <abdelrahimabuelmaaref@gmail.com>
2025-06-20 22:41:50 +0300

    create migrations for service_categories, services, working_hours, time_slots, appointments, and ratings

---
+++ database/migrations/2025_06_20_193857_create_working_hours_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('working_hours', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('provider_id')->constrained('users')->onDelete('cascade');
+            $table->string('day_of_week'); // e.g., 'Monday'
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->time('break_start')->nullable();
+            $table->time('break_end')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('working_hours');
+    }
+};
---
+++ database/migrations/2025_06_20_193946_create_appointments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('provider_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('service_id')->constrained('services')->onDelete('cascade');
+            $table->foreignId('time_slot_id')->constrained('time_slots')->onDelete('cascade');
+            $table->enum('status', ['booked', 'cancelled', 'completed'])->default('booked');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
